e540369ecf1b1ef4b2fea850bd84af156aabc12c
phdljr <phdljr@naver.com>
2023-11-17 22:04:34 +0900

    refactor: modify exception code

--- src/main/java/com/phdljr/todocard/service/impl/CardServiceImpl.java
+++ src/main/java/com/phdljr/todocard/service/impl/CardServiceImpl.java
@@ -5,8 +5,8 @@ import com.phdljr.todocard.dto.response.CardResponseDto;
 import com.phdljr.todocard.dto.response.CardsResponseDto;
 import com.phdljr.todocard.entity.Card;
 import com.phdljr.todocard.entity.User;
-import com.phdljr.todocard.exception.NotFoundCardException;
-import com.phdljr.todocard.exception.NotMatchUser;
+import com.phdljr.todocard.exception.type.NotFoundCardException;
+import com.phdljr.todocard.exception.type.NotMatchUserException;
 import com.phdljr.todocard.repository.CardRepository;
 import com.phdljr.todocard.service.CardService;
 import java.util.List;
@@ -26,7 +26,7 @@ public class CardServiceImpl implements CardService {
     @Transactional(readOnly = true)
     public CardResponseDto getCard(final Long cardId) {
         Card card = cardRepository.findById(cardId)
-            .orElseThrow(() -> new NotFoundCardException("할일 카드를 찾지 못하였습니다."));
+            .orElseThrow(NotFoundCardException::new);
         return card.toCardResponseDto();
     }
 
@@ -44,10 +44,10 @@ public class CardServiceImpl implements CardService {
     public CardResponseDto updateCard(final Long cardId, final CardRequestDto cardRequestDto,
         final User user) {
         Card card = cardRepository.findById(cardId)
-            .orElseThrow(() -> new NotFoundCardException("할일 카드를 찾지 못하였습니다."));
+            .orElseThrow(NotFoundCardException::new);
 
         if (!card.getUser().getId().equals(user.getId())) {
-            throw new NotMatchUserException("회원님의 할일 카드가 아닙니다.");
+            throw new NotMatchUserException();
         }
 
         card.update(cardRequestDto);
@@ -61,8 +61,6 @@ public class CardServiceImpl implements CardService {
             .title(cardRequestDto.getTitle())
             .content(cardRequestDto.getTitle())
             .user(user)
-            .isHidden(false)
-            .isFinished(false)
             .build();
         card = cardRepository.save(card);
         return card.toCardResponseDto();
@@ -71,10 +69,10 @@ public class CardServiceImpl implements CardService {
     @Override
     public Long deleteCard(final Long cardId, final User user) {
         Card card = cardRepository.findById(cardId)
-            .orElseThrow(() -> new NotFoundCardException("할일 카드를 찾지 못하였습니다."));
+            .orElseThrow(NotFoundCardException::new);
 
         if (!card.getUser().getId().equals(user.getId())) {
-            throw new NotMatchUserException("회원님의 할일 카드가 아닙니다.");
+            throw new NotMatchUserException();
         }
 
         cardRepository.delete(card);
@@ -85,10 +83,10 @@ public class CardServiceImpl implements CardService {
     @Transactional
     public CardResponseDto toggleHideCard(final Long cardId, final User user) {
         Card card = cardRepository.findById(cardId)
-            .orElseThrow(() -> new NotFoundCardException("할일 카드를 찾지 못하였습니다."));
+            .orElseThrow(NotFoundCardException::new);
 
         if (!card.getUser().getId().equals(user.getId())) {
-            throw new NotMatchUserException("회원님의 할일 카드가 아닙니다.");
+            throw new NotMatchUserException();
         }
 
         card.toggleHide();
@@ -99,10 +97,10 @@ public class CardServiceImpl implements CardService {
     @Transactional
     public CardResponseDto toggleFinishCard(final Long cardId, final User user) {
         Card card = cardRepository.findById(cardId)
-            .orElseThrow(() -> new NotFoundCardException("할일 카드를 찾지 못하였습니다."));
+            .orElseThrow(NotFoundCardException::new);
 
         if (!card.getUser().getId().equals(user.getId())) {
-            throw new NotMatchUserException("회원님의 할일 카드가 아닙니다.");
+            throw new NotMatchUserException();
         }
 
         card.toggleFinish();
